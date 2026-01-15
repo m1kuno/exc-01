7dc255f718bbd2c33bee2b07975f6f8eb24b9409
m1kun0 <kostyashulgaa@gmail.com>
2026-01-15 19:36:42 +0300

    Add files via upload

---
+++ 04.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Num {
+private:
+  int val;
+
+public:
+  Num() {}
+  Num(int x) : val(x) {}
+  int getval() { return val; }
+  void print() { cout << val; }
+};
+
+class Word {
+private:
+  char str[100];
+  Num id;
+
+public:
+  Word() : id(0) {}
+  Word(const char *str, int x) {
+    id = x;
+    strcpy(this->str, str);
+  }
+  int getid() { return id.getval(); }
+  char *getstr() { return str; }
+  void print() { cout << str; }
+
+  void newstr() {
+    int len = strlen(str);
+    int tar = id.getval();
+    for (int i = tar; i < len - 1; i++) {
+      str[i] = str[i + 1];
+    }
+    str[len - 1] = '\0';
+  }
+};
+int main() {
+  Word arr[2];
+  char s[100];
+  int n;
+  for (auto &x : arr) {
+    cin >> s >> n;
+    x = Word(s, n);
+  }
+  for (int i = 0; i < 2; i++) {
+    arr[i].newstr();
+  }
+  for (int i = 0; i < 2; i++) {
+    arr[i].print();
+  }
+}
